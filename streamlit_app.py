5583dd31748518cbd657465b6d77da1ba0731f71
sanders1973 <136593281+sanders1973@users.noreply.github.com>
2024-12-28 11:08:04 +0000

    g

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,33 +2,27 @@ import streamlit as st
 
 # Initialize session state
 if 'to_do_list' not in st.session_state:
-    st.session_state.to_do_list = []
+    st.session_state.to_do_list = {}
 
 # Title of the app
-st.title('To-Do List')
+st.title('To-Do List with Sub-Items')
 
 # Input text box to add new items
 new_item = st.text_input('Add a new to-do item')
 
-# Add button
-if st.button('Add'):
+# Add button for main items
+if st.button('Add Main Item'):
     if new_item:
-        st.session_state.to_do_list.append(new_item)
-       
+        st.session_state.to_do_list[new_item] = []
+        st.experimental_rerun()
 
 # Display the to-do list with radio buttons to select an item
-selected_item = st.radio('Select an item to modify or delete:', st.session_state.to_do_list)
+selected_main_item = st.radio('Select a main item to modify or delete:', list(st.session_state.to_do_list.keys()))
 
-if selected_item:
-    # Modify item
-    modified_item = st.text_input('Modify selected item', selected_item)
-    if st.button('Update'):
-        index = st.session_state.to_do_list.index(selected_item)
-        st.session_state.to_do_list[index] = modified_item
-        
+if selected_main_item:
+    # Modify main item
+    modified_main_item = st.text_input('Modify selected main item', selected_main_item)
+    if st.button('Update Main Item'):
+        st.session_state.to_do_list[modified_main_item] = st.session_state.to_do_list.pop(selected_main_item)
+        st.experimental_rerun()
     
-    # Delete item
-    if st.button('Delete'):
-        st.session_state.to_do_list.remove(selected_item)
-       
-
