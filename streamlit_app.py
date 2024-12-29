0f4cb5b2a8da4e146380ae0352ae9917e8f2a33b
sanders1973 <136593281+sanders1973@users.noreply.github.com>
2024-12-29 02:01:07 +0000

    vv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,13 @@
 import streamlit as st
 
-# Initialize session state if not already done
-if 'todo_list' not in st.session_state:
-    st.session_state.todo_list = []
+# Title of the app
+st.title("Select an Item from the List")
 
-def add_todo_item():
-    new_item = st.session_state['new_item']
-    if new_item:
-        st.session_state.todo_list.append(new_item)
-        st.session_state['new_item'] = '' # Clear input box after adding
+# List of text items
+items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
 
-def delete_item(index):
-    del st.session_state.todo_list[index]
+# Dropdown menu for selecting an item
+selected_item = st.selectbox("Choose an item:", items)
 
-def modify_item(index, new_text):
-    st.session_state.todo_list[index] = new_text
-
-# Sidebar for entering new items
-st.sidebar.header("Add New Item")
-st.sidebar.text_input("New to-do item", key='new_item')
-st.sidebar.button("Add", on_click=add_todo_item)
-
-# Main page
-st.title("To-Do List")
-
-for i, item in enumerate(st.session_state.todo_list):
-    col1, col2, col3 = st.columns([6, 1, 1])
-    col1.write(item)
-    if col2.button("Modify", key=f'modify_{i}'):
-        new_text = st.text_input("Modify item", value=item, key=f'input_{i}')
-        if st.button("Save", key=f'save_{i}'):
-            modify_item(i, new_text)
-    if col3.button("Delete", key=f'delete_{i}'):
-        delete_item(i)
-
-# Display updated to-do list
-st.write(st.session_state.todo_list)
+# Display the selected item
+st.write(f"You selected: {selected_item}")
