48642406447446136b4d8b2836127ab897892386
sanders1973 <136593281+sanders1973@users.noreply.github.com>
2024-12-29 01:54:22 +0000

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ if 'tasks' not in st.session_state:
 
 # Function to add a new task
 def add_task():
-    if st.session_state['new_task']:
+    if st.session_state['new_task'] and st.session_state['new_description']:
         st.session_state['tasks'].append({
             'task': st.session_state['new_task'],
             'description': st.session_state['new_description']
@@ -19,24 +19,27 @@ def delete_task(index):
     st.session_state['tasks'].pop(index)
 
 # Function to update a task
-def update_task(index):
-    st.session_state['tasks'][index]['task'] = st.session_state[f'task_{index}']
-    st.session_state['tasks'][index]['description'] = st.session_state[f'description_{index}']
+def update_task(index, updated_task, updated_description):
+    st.session_state['tasks'][index]['task'] = updated_task
+    st.session_state['tasks'][index]['description'] = updated_description
 
-st.title("To-Do List Application")
+st.sidebar.title("Add New Task")
+st.sidebar.text_input("Task", key='new_task')
+st.sidebar.text_area("Description", key='new_description')
+st.sidebar.button("Add Task", on_click=add_task)
 
-st.text_input("New Task", key='new_task')
-st.text_area("Description", key='new_description')
-st.button("Add Task", on_click=add_task)
+st.title("To-Do List")
 
 for i, task in enumerate(st.session_state['tasks']):
-    with st.expander(task['task'], expanded=True):
-        st.text_input("", value=task['task'], key=f'task_{i}', on_change=update_task, args=(i,))
-        st.text_area("", value=task['description'], key=f'description_{i}', on_change=update_task, args=(i,))
-        st.button("Delete Task", key=f'delete_{i}', on_click=delete_task, args=(i,))
+    with st.expander(task['task']):
+        st.text_area("Edit Task", value=task['task'], key=f'task_{i}')
+        st.text_area("Edit Description", value=task['description'], key=f'description_{i}')
+        if st.button("Update Task", key=f'update_{i}'):
+            update_task(i, st.session_state[f'task_{i}'], st.session_state[f'description_{i}'])
+        if st.button("Delete Task", key=f'delete_{i}'):
+            delete_task(i)
 
 if st.session_state['tasks']:
     st.success("Tasks updated successfully!")
 
 st.caption("© Your To-Do List App")
-
