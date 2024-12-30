1a2c9468b5aa3829959d955ad6a40ec42a48181e
sanders1973 <136593281+sanders1973@users.noreply.github.com>
2024-12-30 11:34:11 +0000

    nn

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,10 +100,10 @@ def load_github_info():
 
 # Load GitHub info from browser storage on start
 #if st.session_state.github_info == "":
-st.success(st.session_state.github_info, icon="✅")
+
 if st.session_state.github_info_loaded == False:
     load_github_info()
-
+    st.success("here", icon="✅")
 
 if st.session_state.github_info_loaded:
     read_from_github()
