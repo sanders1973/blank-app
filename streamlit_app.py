7473deb261b8687e24e9bc647a8a86bb63cd0059
sanders1973 <136593281+sanders1973@users.noreply.github.com>
2024-12-30 08:17:45 +0000

    fff

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def add_item():
         st.session_state.lists[st.session_state.current_list].append({"Item": st.session_state.item, "Description": st.session_state.description})
         st.session_state.item = ''
         st.session_state.description = ''
-        st.experimental_rerun()
+        st.rerun()
 
 def modify_item(selected_item):
     if selected_item:
@@ -28,21 +28,21 @@ def modify_item(selected_item):
         description = st.sidebar.text_area("Description", value=selected_item["Description"], key='modify_description')
         if st.sidebar.button("Save"):
             st.session_state.lists[st.session_state.current_list][idx] = {"Item": item, "Description": description}
-            st.experimental_rerun()
+            st.rerun()
 
 def delete_item(selected_item):
     if selected_item:
         idx = st.session_state.lists[st.session_state.current_list].index(selected_item)
         if st.sidebar.button("Delete"):
             del st.session_state.lists[st.session_state.current_list][idx]
-            st.experimental_rerun()
+            st.rerun()
 
 def move_item(selected_item, target_list):
     if selected_item:
         idx = st.session_state.lists[st.session_state.current_list].index(selected_item)
         item = st.session_state.lists[st.session_state.current_list].pop(idx)
         st.session_state.lists[target_list].append(item)
-        st.experimental_rerun()
+        st.rerun()
 
 # Sidebar for list selection and item management
 with st.sidebar.expander("Select a List", expanded=True):
